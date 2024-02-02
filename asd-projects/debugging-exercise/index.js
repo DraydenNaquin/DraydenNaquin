ba2c7a6a080b843c6b01a43fc8018358475336ba
Drayden <Naquin693@stcharles.k12.la.us>
2024-02-02 22:49:15 +0000

    saving debugging exercise

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -48,10 +48,10 @@ function makeCircle(id){
     
     // this gives the circle object all of the data that it needs to store
     circle.id = "#" + id;
-    circle.x = Math.random() * maXX + circleRadius;
+    circle.x = Math.random() * maxX + circleRadius;
     circle.y = Math.random() * maxY + circleRadius;
     circle.speedX = decideSpeed();
-    circle.speedY = desideSpeed();
+    circle.speedY = decideSpeed();
     
     return circle;
 }
@@ -109,7 +109,7 @@ function update() {
 
 // this moves circles in memory but doesn't update them on the screen
 function moveCircle(circle){
-    circle.x = circle.speedX;
+    circle.x += circle.speedX;
     circle.y += circle.speedY;
 }
 
@@ -118,29 +118,29 @@ function bounceCircle(circle){
 
     // this bounces off the left wall
     if (circle.x < 0) {
-        circle.x -= circle.speedX;
+        circle.x = 0;
         circle.speedX *= -1;
     }
     // this bounces off the right wall
     else if (circle.x > boardWidth){
-        circle.x -= circle.speedX;
+        circle.x = boardWidth;
         circle.speedX *= -1;
     }
     // this bounces off the top wall
     if (circle.y < 0){
-        circle.y -= circle.speedY;
+        circle.y = 0;
         circle.speedY *= -1;
     }
     // this bounces off the bottom wall
     else if (circle.y > boardHeight){
-        circle.y -= circle.speedY;
-        circle.speedX *= -1;
+        circle.y = boardHeight;
+        circle.speedY *= -1;
     }
 }
 
 // this redraws the circle's position on the screen
 function updateCircleOnScreen(circle){
-    maxCircles = 0;
+    maxCircles = 10;
 
     // these lines redraw the circle's position
     $(circle.id).css('left', circle.x);
