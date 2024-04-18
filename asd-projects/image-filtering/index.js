73682db3f7cb3bad74b29fe5bdf136ea177201d1
Drayden <Naquin693@stcharles.k12.la.us>
2024-04-18 15:38:28 +0000

    saving sorting exercise

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,8 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-  applyFilter(reddify);
-  applyFilterNoBackground(decreaseBlue);
+  // applyFilter(reddify);
+  // applyFilterNoBackground(decreaseBlue);
   applyFilterNoBackground(increaseGreenByBlue);
   // do not change the below line of code
   render($("#display"), image);
